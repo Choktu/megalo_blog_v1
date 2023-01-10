ecd70dc759202b05c254e710c12c883fc55aa5d7
test <abc@163.com>
2023-01-10 22:38:00 +0800

    初始化

---
+++ megalo_base/src/main/java/com/cn/megalo/base/MegaloBaseApplication.java
@@ -0,0 +1,13 @@
+package com.cn.megalo.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaloBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaloBaseApplication.class, args);
+    }
+
+}
---
+++ megalo_base/src/test/java/com/cn/megalo/base/MegaloBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.megalo.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaloBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
